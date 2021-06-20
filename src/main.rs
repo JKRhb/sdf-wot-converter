42e327e87c1bd8230f83a6fc35fc3b2b05fa3937
Jan Romann <jan.romann@uni-bremen.de>
2021-06-20 18:34:16 +0200

    Auto-format main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod sdf;
 mod wot;
 use sdf::definitions::SDFModel;
-use wot::definitions::Thing;
-use std::fs;
 use serde_json::Result;
+use std::fs;
+use wot::definitions::Thing;
 
 fn test_function<T: serde::Serialize + serde::de::DeserializeOwned>(path: &str) -> Result<String> {
-  let example = fs::read_to_string(&path)
-  .expect("Something went wrong reading the file");
+  let example = fs::read_to_string(&path).expect("Something went wrong reading the file");
 
   let sdf_model: T = serde_json::from_str(&example)?;
 
@@ -24,15 +23,14 @@ fn wot_test_function() -> Result<String> {
   return test_function::<Thing>("examples/wot/example.td.json");
 }
 
-
 fn main() {
-    match sdf_test_function() {
-        Ok(result)  => println!("{}", result),
-        Err(e) => println!("{}", e),
-    };
+  match sdf_test_function() {
+    Ok(result) => println!("{}", result),
+    Err(e) => println!("{}", e),
+  };
 
-    match wot_test_function() {
-        Ok(result)  => println!("{}", result),
-        Err(e) => println!("{}", e),
-    };
+  match wot_test_function() {
+    Ok(result) => println!("{}", result),
+    Err(e) => println!("{}", e),
+  };
 }
