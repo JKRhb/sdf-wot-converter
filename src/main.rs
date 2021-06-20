85757d4d9c98e67a2cf6d662f3afaf0c026b8f1a
Jan Romann <jan.romann@uni-bremen.de>
2021-06-20 18:41:58 +0200

    Refactor SDF and WoT test

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use serde_json::Result;
 use std::fs;
 use wot::definitions::Thing;
 
-fn test_function<T: serde::Serialize + serde::de::DeserializeOwned>(path: &str) -> Result<String> {
+fn read_json<T: serde::Serialize + serde::de::DeserializeOwned>(path: &str) -> Result<String> {
   let example = fs::read_to_string(&path).expect("Something went wrong reading the file");
 
   let definition: T = serde_json::from_str(&example)?;
@@ -15,22 +15,14 @@ fn test_function<T: serde::Serialize + serde::de::DeserializeOwned>(path: &str) 
   return Ok(j);
 }
 
-fn sdf_test_function() -> Result<String> {
-  return test_function::<SDFModel>("examples/sdf/example.sdf.json");
-}
-
-fn wot_test_function() -> Result<String> {
-  return test_function::<Thing>("examples/wot/example.td.json");
+fn test_function<T: serde::Serialize + serde::de::DeserializeOwned>(path: &str) -> () {
+  match read_json::<T>(path) {
+    Ok(result) => println!("{}", result),
+    Err(e) => println!("{}", e),
+  };
 }
 
 fn main() {
-  match sdf_test_function() {
-    Ok(result) => println!("{}", result),
-    Err(e) => println!("{}", e),
-  };
-
-  match wot_test_function() {
-    Ok(result) => println!("{}", result),
-    Err(e) => println!("{}", e),
-  };
+  test_function::<SDFModel>("examples/sdf/example.sdf.json");
+  test_function::<Thing>("examples/wot/example.td.json");
 }
