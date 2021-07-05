d21eedd88e6cedce4033a1ab288853d84c21b4c5
Jan Romann <jan.romann@uni-bremen.de>
2021-07-05 03:12:51 +0200

    Update library documentation header

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-//! This crate provides a converter from [SDF](https://datatracker.ietf.org/doc/html/draft-ietf-asdf-sdf-05) (including protocol bindings) to [WoT TD](https://www.w3.org/TR/wot-thing-description/).
+//! This crate provides a converter from
+//! [SDF](https://datatracker.ietf.org/doc/html/draft-ietf-asdf-sdf-05)
+//! (including protocol bindings) to [WoT TD](https://www.w3.org/TR/wot-thing-description/).
 //!
-//! In the end, the converter is both usable as a library that can be built upon in other WoT and SDF related projects as well as a tool for the command line .
+//! The converter is both usable as a library that can be built upon in other WoT
+//! and SDF related projects as well as a tool for the command line.
 
 pub mod converter;
 pub mod sdf;
