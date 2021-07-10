ba8f83cb5597a4852160fc632997f831999c792b
Jan Romann <jan.romann@uni-bremen.de>
2021-07-10 17:22:00 +0200

    TM -> SDF: Add initial @context to namespace mapping

--- src/sdf/sdf_from_tm.rs
+++ src/sdf/sdf_from_tm.rs
@@ -1,5 +1,6 @@
 use super::definitions as sdf;
 use crate::wot::definitions as wot;
+use std::collections::HashMap;
 
 /// Creates an info block from a Thing Model. I am a bit unsure how to map a
 /// TM that has not been an SDF model before therefore this function only
@@ -23,11 +24,32 @@ fn create_info_block(thing_model: &wot::ThingModel) -> Option<sdf::InfoBlock> {
     }
 }
 
+/// Converts the @context of a Thing Model to SDF namespaces. Context entries that
+/// only consist of a single URI are ignored for now as it is unclear how to map them.
+fn convert_namespaces(context: &wot::Context) -> Option<HashMap<String, String>> {
+    let mut namespaces: HashMap<String, String> = HashMap::new();
+    if let wot::Context::Array(array_context) = context {
+        for context_entry in array_context {
+            if let wot::ContextEntry::Map(map_entry) = context_entry {
+                for (key, value) in map_entry {
+                    namespaces.insert(key.clone(), value.clone());
+                }
+            }
+        }
+    }
+
+    if !namespaces.is_empty() {
+        Some(namespaces)
+    } else {
+        None
+    }
+}
+
 /// Converts a WoT Thing Model into an SDF model.
 impl From<wot::ThingModel> for sdf::SDFModel {
     fn from(thing_model: wot::ThingModel) -> Self {
         let info = create_info_block(&thing_model);
-        let namespace = None;
+        let namespace = convert_namespaces(&thing_model.base_thing.context);
         let default_namespace = None;
         let sdf_thing = None;
         let sdf_product = None;
