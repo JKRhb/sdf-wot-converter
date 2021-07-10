3f0d32cfa874601a7fc7a2ca3c7c426cb13247d6
Jan Romann <jan.romann@uni-bremen.de>
2021-07-10 22:16:27 +0200

    Move all read and write operations to the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use sdf_wot_converter::{converter, ConverterResult};
 
 use clap::{crate_authors, crate_version, App, Arg};
-use std::env;
+use std::{env, fs};
 // use url::Url;
 
 fn is_valid_input(input: String) -> Result<(), String> {
@@ -16,16 +16,34 @@ fn is_valid_input(input: String) -> Result<(), String> {
     }
 }
 
+fn get_json_from_file(path: &str) -> ConverterResult<String> {
+    fs::read_to_string(&path).map_err(|e| e.into())
+}
+
 fn print_model_from_file(path: &str) -> ConverterResult<()> {
+    let json_string = get_json_from_file(path)?;
     if path.ends_with("sdf.json") {
-        converter::print_sdf_definition_from_path(path)
+        converter::print_sdf_definition(json_string)
     } else if path.ends_with("td.json") {
-        converter::print_wot_td_definition_from_path(path)
+        converter::print_wot_td_definition(json_string)
     } else if path.ends_with("tm.json") {
-        converter::print_wot_tm_definition_from_path(path)
+        converter::print_wot_tm_definition(json_string)
     } else {
         Err("Illegal path ending!".into())
     }
+}
+
+fn convert_model_from_file_to_file(input_path: &str, output_path: &str) -> ConverterResult<()> {
+    let input_string = get_json_from_file(input_path)?;
+    let output_string: String;
+    if input_path.ends_with("sdf.json") {
+        output_string = converter::convert_sdf_to_wot_tm(input_string)?;
+    } else if input_path.ends_with("tm.json") {
+        output_string = converter::convert_wot_tm_to_sdf(input_string)?;
+    } else {
+        return Err("TD to SDF conversion is not implemented yet!".into());
+    }
+    fs::write(output_path, output_string).map_err(|e| e.into())
 }
 
 fn main() -> ConverterResult<()> {
@@ -72,19 +90,16 @@ fn main() -> ConverterResult<()> {
 
     if let Some(ref matches) = app.subcommand_matches("print") {
         if let Some(path) = matches.value_of("input") {
-            return print_model_from_file(path);
+            print_model_from_file(path)?
         }
     } else if let Some(ref matches) = app.subcommand_matches("convert") {
-        // TODO: Replace if-else with match
-        let input_path = matches.value_of("input").unwrap();
-        let output_path = matches.value_of("output").unwrap();
-        if input_path.ends_with("sdf.json") {
-            return converter::sdf_to_wot_from_and_to_path(input_path, output_path);
-        } else if input_path.ends_with("tm.json") {
-            return converter::wot_tm_to_sdf_from_and_to_path(input_path, output_path);
-        } else {
-            return Err("TD to SDF conversion is not implemented yet!".into());
-        }
+        let input_path = matches
+            .value_of("input")
+            .ok_or_else(|| "No input path found!".to_string())?;
+        let output_path = matches
+            .value_of("output")
+            .ok_or_else(|| "No output path found!".to_string())?;
+        convert_model_from_file_to_file(input_path, output_path)?
     }
 
     Ok(())
