1134d38a1fc851dd5f6f00c6ad366d7e9516d800
Jan Romann <jan.romann@uni-bremen.de>
2021-07-13 22:23:11 +0200

    SDF -> WoT TM: Refactor writable/readable mapping

--- src/wot/tm_from_sdf.rs
+++ src/wot/tm_from_sdf.rs
@@ -245,24 +245,24 @@ fn map_data_type(jsonschema: &Option<sdf::Types>) -> Option<wot::JSONSchemaTypes
     }
 }
 
+/// Maps SDF's `readable` and `writable` to WoT TD's `writeOnly` and `readOnly`.
+///
+/// # Return value
+///
+/// A boolean tuple. The first value represents `writeOnly`, the second one `readOnly`.
+fn map_readable_writable(
+    readable: Option<bool>,
+    writable: Option<bool>,
+) -> (Option<bool>, Option<bool>) {
+    let write_only = readable.map(|x| !x);
+    let read_only = writable.map(|x| !x);
+    (write_only, read_only)
+}
+
 fn convert_to_data_schema(sdf_property: &sdf::DataQualities) -> wot::DataSchema {
     // TODO: How should nullable be mapped?
-
-    let write_only;
-    let read_only;
-    let writable = sdf_property.writable.unwrap_or(true);
-    let readable = sdf_property.readable.unwrap_or(true);
-    if !readable && writable {
-        write_only = Some(true);
-        read_only = None;
-    } else if !writable && readable {
-        write_only = None;
-        read_only = Some(true);
-    } else {
-        // TODO: How do you map a property that is neither writable nor readable?
-        write_only = None;
-        read_only = None;
-    }
+    let (write_only, read_only) =
+        map_readable_writable(sdf_property.readable, sdf_property.writable);
 
     wot::DataSchema {
         write_only,
