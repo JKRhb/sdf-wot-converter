438a0b21535ad43dd5a0bf91e7a5fe6c0249c50e
Jan Romann <jan.romann@uni-bremen.de>
2021-07-20 19:29:09 +0200

    SDF -> TM: Add integration test for data types

--- tests/converter.rs
+++ tests/converter.rs
@@ -24,6 +24,88 @@ fn test_empty_sdf_tm_conversion() {
     test_sdf_wot_conversion(sdf_input, expected_tm_result);
 }
 
+#[test]
+fn test_sdf_tm_type_conversion() {
+    let sdf_input = r#"{
+        "sdfProperty": {
+            "foo": {
+                "type": "integer",
+                "minimum": 0,
+                "maximum": 9002,
+                "exclusiveMinimum": 0,
+                "exclusiveMaximum": 9000,
+                "multipleOf": 2
+            },
+            "bar": {
+                "type": "number",
+                "minimum": 0.0,
+                "maximum": 9002.0,
+                "exclusiveMinimum": 0.0,
+                "exclusiveMaximum": 9000.0,
+                "multipleOf": 2.0
+            },
+            "baz": {
+                "type": "string",
+                "minLength": 3,
+                "maxLength": 5,
+                "pattern": "email"
+            },
+            "foobar": {
+                "type": "array",
+                "minItems": 2,
+                "maxItems": 5
+            },
+            "barfoo": {
+                "type": "object",
+                "required": ["foo"]
+            }
+        }
+    }"#;
+    let expected_tm_result = json!(
+        {
+            "@context": [
+              "https://www.w3.org/2019/wot/td/v1"
+            ],
+            "@type": "Thing",
+            "properties": {
+                "foo": {
+                    "type": "integer",
+                    "minimum": 0,
+                    "maximum": 9002,
+                    "exclusiveMinimum": 0,
+                    "exclusiveMaximum": 9000,
+                    "multipleOf": 2
+                },
+                "bar": {
+                    "type": "number",
+                    "minimum": 0.0,
+                    "maximum": 9002.0,
+                    "exclusiveMinimum": 0.0,
+                    "exclusiveMaximum": 9000.0,
+                    "multipleOf": 2.0
+                },
+                "baz": {
+                    "type": "string",
+                    "minLength": 3,
+                    "maxLength": 5,
+                    "pattern": "email"
+                },
+                "foobar": {
+                    "type": "array",
+                    "minItems": 2,
+                    "maxItems": 5
+                },
+                "barfoo": {
+                    "type": "object",
+                    "required": ["foo"]
+                }
+            }
+        }
+    );
+
+    test_sdf_wot_conversion(sdf_input, expected_tm_result);
+}
+
 fn test_wot_tm_sdf_conversion(wot_tm_input: &str, expected_result: serde_json::Value) {
     let result = converter::convert_wot_tm_to_sdf(wot_tm_input.to_string()).unwrap();
     let result_json: serde_json::Value = serde_json::from_str(result.as_str()).unwrap();
